1742c5b202ac39c4956bfc9f48dc77aac92e0d06
PrateekLC <lamichhaneprateek@gmail.com>
2024-02-07 16:58:36 +0545

    frontend&views

--- home/views.py
+++ home/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.views import View
+from .models import *
 
 # Create your views here.
 class BaseView(View):
     views = {}
 
+    views['category'] = category.objects.all()
+    views['brand'] = brand.objects.all()
+    views['contact'] = contactinfo.objects.all()
+
 class HomeView(BaseView):
     def get(self,request):
-
-        return render(request,'index.html')
+        self.views
+        self.views['sliders'] = slider.objects.all()
+        self.views['ad'] = ad.objects.all()
+        self.views['product'] = product.objects.all()
+        return render(request,'index.html',self.views)
